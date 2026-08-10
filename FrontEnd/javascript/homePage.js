2e8efa79fa012d86fa2999725a61ea7b7bbc625d
Upander garg <Upander1749.becse24@chitkara.edu.in>
2026-08-10 15:59:53 +0530

    Adding Landing Page && defining overall structure and limits of the game- Day1

---
+++ FrontEnd/javascript/homePage.js
@@ -0,0 +1,21 @@
+
+        const playBtn = document.getElementById("playBtn");
+        const guideBtn = document.getElementById("guideBtn");
+        const shopBtn = document.getElementById("shopBtn");
+        const statsBtn = document.getElementById("statsBtn");
+
+        playBtn.addEventListener("click", () => {
+            console.log("Starting Ragdoll Archer...");
+        });
+
+        guideBtn.addEventListener("click", () => {
+            console.log("Opening Guide...");
+        });
+
+        shopBtn.addEventListener("click", () => {
+            console.log("Opening Shop...");
+        });
+
+        statsBtn.addEventListener("click", () => {
+            console.log("Opening Stats...");
+        });
